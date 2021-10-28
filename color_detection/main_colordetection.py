3458b139315f8dbe85b0d858a1c39d384b982514
Loïc ABEL <loic.abel@hotmail.com>
2021-10-28 19:45:18 +0200

    Path change for color_detection main

--- color_detection/main_colordetection.py
+++ color_detection/main_colordetection.py
@@ -4,12 +4,12 @@ import time
 
 def main():
     start_time = time.time()
-    detector = ColorDetector(paths=["Color detection/images/blanc.jpg",
-                                    "Color detection/images/bleu.jpg",
-                                    "Color detection/images/jaune.jpg",
-                                    "Color detection/images/orange.jpg",
-                                    "Color detection/images/rouge.jpg",
-                                    "Color detection/images/vert.jpg"],
+    detector = ColorDetector(paths=["color_detection/images/blanc.jpg",
+                                    "color_detection/images/bleu.jpg",
+                                    "color_detection/images/jaune.jpg",
+                                    "color_detection/images/orange.jpg",
+                                    "color_detection/images/rouge.jpg",
+                                    "color_detection/images/vert.jpg"],
                              show_rgb=False, show_images=False)
     detector.processImages()
     print("--- %s seconds ---" % (time.time() - start_time))
